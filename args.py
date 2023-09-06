631d71dde3cce8b019754ed4e7f0987d5e5aabc7
zhangzheng0908 <112859328+zhangzheng0908@users.noreply.github.com>
2023-09-06 18:33:47 +0800

    Add files via upload

---
+++ args.py
@@ -0,0 +1,22 @@
+## CONFIGS ###
+dataset = 'pubmed'
+model = 'GAE'
+
+###input_dim = 1433 
+###hidden1_dim = 32*2
+###hidden2_dim = 16*2
+###use_feature = True
+
+###num_epoch = 650
+###learning_rate = 0.01125
+
+
+
+input_dim = 1433
+input_dim = 500
+hidden1_dim = 32*2
+hidden2_dim = 16*2
+use_feature = True
+
+num_epoch = 200
+learning_rate = 0.015137527525
